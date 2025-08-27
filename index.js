584f6e0eeeb111b34bb055423e08a37dbfa13574
Lunisito99 <153399475+Lunisito99@users.noreply.github.com>
2025-08-27 14:42:54 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "servidor-b3f89",
+  "private_key_id": "7fbb32e1ac1a678b804790cfd384d3a045aeb99c",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNtQthYTwWpTNd\nSuQSLC1LAS0BUsxbprmhY9g0JXxTGCr7P3f+6B+Hv7lcUPna/T02NJHcYAYnKe4D\nL/mOQOMYekO+27D9pkhG6v3tDyLoN5+FwlcXMsCDyvPc/OOUvth6ntj55On/cmF+\nsYI8ZCsC3uI+Vx2uPjzxPV/KoJBQps7/oX/28rfFLwf+FSWQ8v09Q+WYMOvjezEV\nuZQJn9yeCrYIxjEjG/xZRF73RSkjb7V6uG7h3Z9H03QTguO1qrr0XvQ0rXiZQHWh\noLVHHMk1deMfBMPeRzeiMTc8XEpnH7tTgZfGPy4iTxNDufn0hXZuej6DO6xXaXfj\ncFadkwg5AgMBAAECggEABSrQUZ6l6gK11tjYAE3cSGdL6F3aVM4MGS3/IxYy+Iph\noASQ4NYXXqtV/mfpUtMsK7Wn2VOPLAItYKaem/Bs0/NUStHNcR2xcJicDs2jyJYH\nG47Nbb+AXv8k3475r4WYvgHJ/QL/TsOih9JxLU+t1elf4QWCsRsOoFe0lYwxe9kd\nQ2m32Q/Qyi8iJZOlEgDscERNSwOWtPJ3XAv1yMKjpgyOQILriDJOXTqov08AKqQw\nC8at5rjS800Pv8gPYiRZtQ/vTMbjW1MSKggSZis8ul3ZoA2XS81kqlD29IYSNE6w\nMoKWd0bB5iFaAX1cDnXABAN7nXViUsimuypmcvN4gQKBgQD7xf1xrP6KMq94AfsX\n439w/6jeNXR3H/ugWsAWqnkTC5GDbo7eO0bpCGjbclIYZ9oyH4kqhRXJ0dQyZ84f\nlb5GHaYrB0k2p50xh3hxcvBXrH8ls0JnIDrOxpHfIt0pUUZmqmHv9oEWx78H0IYN\ns+7+P2mHmgXZK8pTsQ5Zxsw4eQKBgQDRKRUbMO5Uroo9pq8o6B3Vr+c3iwfDj2qG\nRUkOikbzwzFEyTrG7trvsyIJ9VdKyrDQYColjIZC/qUsKF98IiRiRP3Z+lFhwB7k\n6s7mN11Ts2ePXh5x1HNb4SlZ96uRdOEQ/DJb1F96Y5T1+eQaT1Alagpvdy+3Yvw/\nZlcBuLzdwQKBgHkJW6gUdGHXsAdPXHjFg3S9U19P72d9VqhmOEYtOftv6kp3EfPA\nJ3zL8CzfLpxRANJr2rM4O5tEZnC8+piwkkJ+6m1xGT9PIQC4kaxohyZ+5LkpDlkp\nY1wYlfPhfNTqH6AndOG5baGX8Tw+gNOBlvlfXsozv3AB30iQq8d5DKBxAoGAb6Ad\nQ4W5rDf6TyzGDm7SfViDbYElg5jmoQIZG0lw1Gc3xB4g4EpZTdOsNuRY0fGaq5qm\n46M7hEWioQ5q5zTRdiCb2dld1W1TD5jBEtw5UpJMkpcIaHeZmU9xYjJrvhVCiG4P\n8jJFQmwi86OJuQDbJRyStSpGrPiG5s8XYDPXmAECgYEA9k0Nnfxp87hIug08XlJl\n5zVpaVM6mKsxHN9qgyi6PvagRdzdYjWgN9Ror4TnZcg37qW12jDPrJw1TD2HwSAI\nROkrQTWJxNBmj0hUbouudptaMg6jKM1z5Ccnu+gZFtS0O3oD2dlVc3L1ZL03iim3\n8CAGiN813HC6bf1si5eaDvY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@servidor-b3f89.iam.gserviceaccount.com",
+  "client_id": "117396326532534481153",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40servidor-b3f89.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
